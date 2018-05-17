1886ff9d5c4534c177b5d2b1f6738bfa0f638768
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-05-17 14:12:43 -0500

    Part-1

--- index.js
+++ index.js
@@ -1,61 +1,44 @@
 (function() {
   'use strict';
-let stopButton = document.getElementById('stopButton');
-let slowButton = document.getElementById('slowButton');
-let goButton = document.getElementById('goButton');
-let stopLight = document.getElementById('stopLight');
-let slowLight = document.getElementById('slowLight');
-let goLight = document.getElementById('goLight');
-let stopButtonOn = false;
-let slowButtonOn = false;
-let goButtonOn = false;
+  let stopButton = document.getElementById('stopButton');
+  let slowButton = document.getElementById('slowButton');
+  let goButton = document.getElementById('goButton');
+  let stopLight = document.getElementById('stopLight');
+  let slowLight = document.getElementById('slowLight');
+  let goLight = document.getElementById('goLight');
+  let stopButtonOn = false;
+  let slowButtonOn = false;
+  let goButtonOn = false;
 
-stopButton.addEventListener('click', function() {
- if (stopButtonOn===true) {
-stopLight.style.backgroundColor = 'black';
-stopButtonOn = false;
-} else {
-  stopLight.style.backgroundColor = 'red';
-  stopButtonOn = true;
-}
-});
+  stopButton.addEventListener('click', function() {
+    if (stopButtonOn) {
+      stopLight.style.backgroundColor = 'black';
+      stopButtonOn = false;
+    } else {
+      stopLight.style.backgroundColor = 'red';
+      stopButtonOn = true;
+    }
+  });
 
-slowButton.addEventListener('click', function() {
- if (slowButtonOn===true) {
-slowLight.style.backgroundColor = 'black';
-slowButtonOn = false;
-} else {
-  slowLight.style.backgroundColor = 'yellow';
-  slowButtonOn = true;
-}
-})
+  slowButton.addEventListener('click', function() {
+    if (slowButtonOn) {
+      slowLight.style.backgroundColor = 'black';
+      slowButtonOn = false;
+    } else {
+      slowLight.style.backgroundColor = 'yellow';
+      slowButtonOn = true;
+    }
+  });
 
-goButton.addEventListener('click', function() {
- if (stopButtonOn===true) {
-goLight.style.backgroundColor = 'black';
-goButtonOn = false;
-} else {
-  goLight.style.backgroundColor = 'green';
-  goButtonOn = true;
-}
-})
+  goButton.addEventListener('click', function() {
+    if (goButtonOn) {
 
+      goLight.style.backgroundColor = 'black';
+      goButtonOn = false;
+    } else {
+      goLight.style.backgroundColor = 'green';
+      goButtonOn = true;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+  });
 })();
